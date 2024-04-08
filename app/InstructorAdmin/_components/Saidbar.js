13fb9fbb4bfb0baadcc16bc3e19808ef59bff82d
Umidjon <127615615+Umidjon-developer05@users.noreply.github.com>
2024-04-08 19:54:12 +0500

    Update Saidbar.js

--- app/InstructorAdmin/_components/Saidbar.js
+++ app/InstructorAdmin/_components/Saidbar.js
@@ -1,64 +1,81 @@
 "use client";
 import React from "react";
-import { motion } from "framer-motion";
-import { LampContainer } from "../../components/ui/lamp";
 import {
   Card,
   CardHeader,
   CardBody,
   CardFooter,
   Divider,
-  Link,
   Image,
+  Button,
 } from "@nextui-org/react";
 import { useSession } from "next-auth/react";
-import { Button } from "@nextui-org/react";
+import { Skeleton } from "@nextui-org/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 export default function LampDemo() {
   const session = useSession();
+  const router = useRouter();
+  function SignOut() {
+    localStorage.removeItem("token");
+
+    router.push("/");
+  }
   return (
-    <LampContainer className={"-mt-20"}>
-      <motion.h1
-        initial={{ opacity: 0.5, y: 100 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-      >
-        <Card className="sm:w-[1000px]   animate-shimmer  items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none  ">
-          <CardHeader className="flex gap-3 ">
+    <Card className="sm:w-[1000px]    rounded-md border border-slate-800  px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2  ">
+      <CardHeader className="flex gap-3  justify-between ">
+        <div className="flex gap-4">
+          <div>
             <Image
               alt="nextui logo"
               height={40}
               radius="full"
-              src={session?.data?.user?.image}
+              src={
+                session?.data?.user?.image ? (
+                  session?.data?.user?.image
+                ) : (
+                  <Skeleton className="flex rounded-full w-12 h-12" />
+                )
+              }
               width={40}
             />
-            <div className="flex flex-col">
-              <p className="text-md">{session?.data?.user?.name}</p>
-              <p className="text-small text-default-500">
-                {session?.data?.user?.email}
-              </p>
-            </div>
-          </CardHeader>
-          <Divider />
-          <CardBody>
-            <p>Xush kelibsiz admin panelga.</p>
-          </CardBody>
-          <Divider />
-          <CardFooter>
-            <Link href="/InstructorAdmin/dashboard" >
-              <Button
-                radius="lg"
-                className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
-              >
-                Dashboard
-              </Button>
-            </Link>
-          </CardFooter>
-        </Card>
-      </motion.h1>
-    </LampContainer>
+          </div>
+          <div className="flex flex-col ">
+            <p className="text-md">
+              {session?.data?.user?.name ? (
+                session?.data?.user?.name
+              ) : (
+                <Skeleton className="h-3 w-full rounded-lg" />
+              )}
+            </p>
+            <p className="text-small text-default-500">
+              {session?.data?.user?.email}
+            </p>
+          </div>
+        </div>
+        <div>
+          <Button
+            onClick={() => SignOut()}
+            className="bg-gradient-to-tr p-2 rounded-lg from-pink-500 to-yellow-500 text-white shadow-lg"
+          >
+            Logout
+          </Button>
+        </div>
+      </CardHeader>
+
+      <Divider />
+      <CardBody>
+        <p>Xush kelibsiz admin panelga.</p>
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <Link
+          href="/InstructorAdmin/dashboard1"
+          className="bg-gradient-to-tr p-2 rounded-lg from-pink-500 to-yellow-500 text-white shadow-lg"
+        >
+          Dashboard
+        </Link>
+      </CardFooter>
+    </Card>
   );
 }
