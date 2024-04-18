b5a2a8e19dd2b0e53c1030677c71842087d4612d
Umidjon <127615615+Umidjon-developer05@users.noreply.github.com>
2024-04-18 20:31:25 +0500

    sad

--- app/InstructorAdmin/dashboard/_components/Product.js
+++ app/InstructorAdmin/dashboard/_components/Product.js
@@ -78,7 +78,7 @@ export default function Product({
 
   return (
     <div>
-      <div className="sm:max-w-[1100px] mx-10" >
+      <div className="sm:w-[1100px] mx-10" >
         {/* Title input */}
         <div className="items-center my-4">
           <label className="text-lg font-medium mb-3">Title</label>
@@ -169,7 +169,7 @@ export default function Product({
            onClick={() => CreateProduct()}
           className="bg-transparent transition-all w-full mt-10 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-4 px-6 border border-blue-500 hover:border-transparent rounded"
         >
-          Save
+          Save work 
         </button>
       </div>
     </div>
