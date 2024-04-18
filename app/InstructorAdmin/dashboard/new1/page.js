0115e815e7c08bd92378c48a7c3ac249be3254ea
Umidjon <127615615+Umidjon-developer05@users.noreply.github.com>
2024-04-18 22:13:07 +0500

    asd

--- app/InstructorAdmin/dashboard/new1/page.js
+++ app/InstructorAdmin/dashboard/new1/page.js
@@ -1,11 +1,17 @@
+import { useSession } from "next-auth/react";
 import Product from "../_components/Product";
 
 export default function NewProduct() {
-  return (
-    <>
-      <section >
-          <Product />
-      </section>
-    </>
-  );
+  const session = useSession();
+  if (session?.status === "authenticated") {
+    return (
+      <>
+        <section className="p-4">
+          <div>
+            <Product />
+          </div>
+        </section>
+      </>
+    );
+  }
 }
